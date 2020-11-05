673c13fe5baebadb55e363569b4a6010428c6146
marcinkegel <kegelmarcin@gmail.com>
2020-11-05 20:35:47 +0100

    sign-up with firebase working

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 import App from './components/App';
 import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
     <FirebaseContext.Provider value={new Firebase()}>
-        <App />
+        <App/>
     </FirebaseContext.Provider>,
     document.getElementById('root'),
 );
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
