1bb6c82f9770349e42c5e017cdc034f04e2b54f3
marcinkegel <kegelmarcin@gmail.com>
2020-11-05 13:01:33 +0100

    authentication method chosen and implemented in the Firebase class

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,7 +1,8 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 
-var firebaseConfig = {
+const firebaseConfig = {
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
@@ -13,8 +14,26 @@ var firebaseConfig = {
 
 class Firebase {
     constructor() {
-        app.initializeApp(config);
+        app.initializeApp(firebaseConfig);
+
+        this.auth = app.auth();
+
     }
+    // *** Auth API ***
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
+
 }
 
 export default Firebase;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const SignUp = () => {
+import * as ROUTES from '../../constants/routes';
 
-    return(
-        <div>
-            <h1>SignUp</h1>
-        </div>
-    );
+const SignUpPage = () => (
+    <div>
+        <h1>SignUp</h1>
+        <SignUpForm />
+    </div>
+);
+
+class SignUpForm extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    onSubmit = event => {
+
+    }
+
+    onChange = event => {
+
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+
+            </form>
+        );
+    }
 }
 
-export default SignUp;
+const SignUpLink = () => (
+    <p>
+        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+);
+
+export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
