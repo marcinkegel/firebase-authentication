42a8992625e4ae32f04d536ba5576af4bd5445a5
marcinkegel <kegelmarcin@gmail.com>
2020-11-10 17:56:52 +0100

    admin page added

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 
 const firebaseConfig = {
@@ -18,6 +19,7 @@ class Firebase {
         app.initializeApp(firebaseConfig);
 
         this.auth = app.auth();
+        this.db = app.database();
 
     }
     // *** Auth API ***
@@ -34,6 +36,12 @@ class Firebase {
 
     doPasswordUpdate = password =>
         this.auth.currentUser.updatePassword(password);
+
+    // *** User API ***
+
+    user = uid => this.db.ref(`users/${uid}`);
+
+    users = () => this.db.ref('users');
 
 }
 
